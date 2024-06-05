26d9df2471aa514a71b7d660c934dc5602a9e762
newlearner2407 <manojbajpeyi804@gmail.com>
2024-06-05 15:17:23 +0530

    first try

---
+++ index.mjs
@@ -0,0 +1,23 @@
+// import add from './add.mjs';
+
+// const results = add(6,6);
+// console.log(results);
+
+// var a= '20';
+// var b= 20;
+// if (a==b) {
+//     console.log("Matched");
+// } else {
+//     console.log("Not Matched");
+// }
+
+// const arr = [
+//     {id:1, name:"palash", price:999},
+//     {id:2, name:"apurwa", price:111},
+//     {id:3, name:"Shruti", price:222},
+// ];
+// let results = arr.filter(number=>number.price!==222);
+
+
+import { writeFileSync } from 'fs';
+writeFileSync("Hello.txt","code stpe bye step")
