2ac25b3735960ac33ecc42bd5f61f940b2673ef8
newlearner2407 <manojbajpeyi804@gmail.com>
2024-06-08 01:02:35 +0530

    api practice

---
+++ data.js
@@ -0,0 +1,7 @@
+
+const data = [
+    {name:'palash Kamble',email:'palashkamble@test.com'},
+    {name:"vishal", email:"vishal@gmail.com"},
+    {name:"raj", email:"raj@gmail.com"}
+]
+module.exports=data;
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const http = require('http');
-
-http.createServer((req,res)=>{
-res.write("<h1>Hello behn le lodo</h1>");
-res.end();
-}).listen(3000);
+const data = require('./data');
+http.createServer((req,resp)=>{
+    resp.writeHead(200,{'Content-Type':'application\json'});
+    resp.write(JSON.stringify({data}));
+    resp.end();
+}).listen(5000);
