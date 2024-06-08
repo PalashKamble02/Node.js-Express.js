594183f090f9db3ea6669033f4d12030ccb866be
newlearner2407 <manojbajpeyi804@gmail.com>
2024-06-08 14:15:46 +0530

    created file in directory and read and listed it

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-const http = require('http');
-const data = require('./data');
-http.createServer((req,resp)=>{
-    resp.writeHead(200,{'Content-Type':'application\json'});
-    resp.write(JSON.stringify({data}));
-    resp.end();
-}).listen(5000);
+const fs = require('fs');
+const path = require('path');
+const dirPath = path.join(__dirname, 'files');
+
+// Create the directory if it doesn't exist
+if (!fs.existsSync(dirPath)){
+    fs.mkdirSync(dirPath);
+}
+
+// Create 5 text files
+for (let i = 0; i < 5; i++) {
+    fs.writeFileSync(path.join(dirPath, `hello${i}.txt`), "a simple text file");
+}
+
+// Read and list files in the directory
+fs.readdir(dirPath, (err, files) => {
+    if (err) {
+        return console.error('Unable to read directory:', err);
+    }
+    files.forEach((item) => {
+        console.log(item);
+    });
+});
