69914ab3ddf1301ff9af475dc2d4472b746af3a8
newlearner2407 <manojbajpeyi804@gmail.com>
2024-06-11 16:16:15 +0530

    Node.js Internal Working

---
+++ nodejsWorking.js
@@ -0,0 +1,11 @@
+console.log("Startig Up");
+
+setTimeout(() => {
+    console.log("two Seconds");
+}, 2000);
+
+setTimeout(() => {
+    console.log("Zero Seconds");
+}, 0);
+
+console.log("Ending Up");
