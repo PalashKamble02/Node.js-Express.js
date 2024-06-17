2afd393b318bb121291a2ad38c985f60effdf800
newlearner2407 <manojbajpeyi804@gmail.com>
2024-06-17 15:53:38 +0530

    removed Extensions from URL

---
+++ removingExtension.js
@@ -0,0 +1,20 @@
+const  express = require('express');
+const path = require('path');
+
+const app = express();
+const publicPath=path.join(__dirname,'public');
+
+//  app.use(express.static(publicPath));
+app.get('',(req,res)=>{
+    res.sendFile(`${publicPath}/index.html`)
+});
+app.get('/about',(req,res)=>{
+    res.sendFile(`${publicPath}/about.html`)
+});
+app.get('/help',(req,res)=>{
+    res.sendFile(`${publicPath}/help.html`)
+});
+app.get('*',(req,res)=>{
+    res.sendFile(`${publicPath}/nopage.html`)
+});
+app.listen(5000);
